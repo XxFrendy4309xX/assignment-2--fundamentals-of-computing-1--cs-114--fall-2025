abf69e88e0faf62bae1c4f7b180a02de8f2874f0
XxFrendy4309xX <139586934+XxFrendy4309xX@users.noreply.github.com>
2025-10-14 18:58:09 -0400

    Commit Three Programs

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -1,4 +1,15 @@
 public class OneHundredBottlesOfBeer {
-  public static void main(String[] args) {
+   public static void main(String[] args) {
+     for (int c = 99; c > 0;  c--) {
+       if  (c > 1) {
+         System.out.println(c + " Bottles of Beer on the Wall,\n"
+	 + c + " Bottles of Beer!\nTake one down, pass it around,\n"+
+	 (c - 1) + " Bottles of Beer on the Wall!");
+       } else {
+         System.out.println(c + " Bottle of Beer on the Wall\n"
+	 + c + " Bottle of Beer!\n Take it down, pass it around, \n"
+	 +"and now there is no more bottles of beer on the wall!!!");
+      }
+    }
   }
 }
--- PrintVowelsAndNonVowels.java
+++ PrintVowelsAndNonVowels.java
@@ -1,4 +1,83 @@
+import java.util.Scanner;
+
 public class PrintVowelsAndNonVowels {
   public static void main(String[] args) {
+	  
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a string containing only alphabetic characters: ");
+    String vowel = scanner.nextLine();
+	  
+    int returnedVowelNumber =
+      getVowelNumber(vowel);
+    if (returnedVowelNumber == 0) {
+      System.out.println("There are no vowels!!!");
+    } else {
+      System.out.println("Amount of Vowels: " + returnedVowelNumber);
+    }
+  }
+  
+  public static int getVowelNumber(String vowel) {
+	  
+    int vowelNumber = 0;
+    int aNumber = 0; // Move all these to globals?
+    int eNumber = 0;
+    int iNumber = 0;
+    int oNumber = 0;
+    int uNumber = 0;
+    int otherNumber = 0;
+    if (vowel == null) {
+      return vowelNumber;
+    }
+	  
+    String aString ="a";
+    String eString = "e";
+    String iString = "i";
+    String oString = "o";
+    String uString = "u";
+	  
+    for (int aIndex = vowel.indexOf(aString); // for "a".
+      aIndex >= 0;
+      aIndex = vowel.indexOf(aString, aIndex + 1)) {
+        aNumber = aNumber + 1;
+        vowelNumber = vowelNumber + 1;
+    }
+		
+    for (int eIndex = vowel.indexOf(eString); // for "e".
+      eIndex >= 0;
+      eIndex = vowel.indexOf(eString, eIndex + 1)) {
+        eNumber = eNumber + 1;
+	vowelNumber = vowelNumber + 1;
+    }
+	  
+    for (int iIndex = vowel.indexOf(iString); // for "i".
+      iIndex >= 0;
+      iIndex = vowel.indexOf(iString, iIndex + 1)) {
+        iNumber = iNumber + 1;
+        vowelNumber = vowelNumber + 1;
+    }
+		
+    for (int oIndex = vowel.indexOf(oString); // for "o".
+      oIndex >= 0;
+      oIndex = vowel.indexOf(oString, oIndex + 1)) {
+        oNumber = oNumber + 1;
+        vowelNumber = vowelNumber + 1;
+    }
+	  
+    for (int uIndex = vowel.indexOf(uString); // for "u".
+      uIndex >= 0;
+      uIndex = vowel.indexOf(uString, uIndex + 1)) {
+        uNumber = uNumber + 1;
+        vowelNumber = vowelNumber + 1;
+    }
+	  
+    System.out.println("Instanes of 'a': " + aNumber);
+    System.out.println("Instanes of 'e': " + eNumber);
+    System.out.println("Instanes of 'i': " + iNumber);
+    System.out.println("Instanes of 'o': " + oNumber);
+    System.out.println("Instanes of 'u': " + uNumber);
+    int len = vowel.length();
+    int nonVowels = (len - vowelNumber);
+    System.out.println("Instanes of non-vowels: " + nonVowels);
+    return vowelNumber;
   }
 }
